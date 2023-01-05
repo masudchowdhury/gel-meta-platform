51051508370076f8b7d05680c9dd5d42d1b3dbf4
masud.chowdhury <mchowdhury.eng@gmail.com>
2023-01-05 17:28:52 +0000

    Remove backend.tf from meta-remote-state

--- meta-remote-state/main.tf
+++ meta-remote-state/main.tf
@@ -30,7 +30,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_b
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "gel-meta-terraform-service-lock-table"
+  name         = "gel-meta-terraform-service-state-lock-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
